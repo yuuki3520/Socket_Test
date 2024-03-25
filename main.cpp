e57569e45f80136ffe34f0695665e25fae10382e
BULIANG <mrliu.4168@gmail.com>
2024-03-25 10:05:33 +0800

    多线程实例1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <thread>
+
+void func(int a){
+    while(true)
+    {
+        std::cout << "Hello World\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+}
 int main()
 {
+    int a = 0;
+    std::thread thread_tmp(func,a);
     std::cout <<"This is a CPP" << std::endl;
+    thread_tmp.join();
     return 0;
 }
