0aba538fc9999adc2bd5854b29d5ce014469df6e
BULIANG <mrliu.4168@gmail.com>
2024-03-29 19:18:26 +0800

    添加注释

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
+/*互斥量*/
+
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+std::mutex mtx1;
+std::mutex mtx2;
+int globalVariable = 0;
+
+ void task1()
+ {
+    for(int i = 0; i < 100000; i++)
+    {
+        std::lock_guard<std::mutex> lock1(mtx1,std::adopt_lock);    //lock_guard 和 lock搭配使用时必须添加 std::adopt_lock参数
+        std::lock_guard<std::mutex> lock2(mtx2,std::adopt_lock);
+        std::lock(mtx1,mtx2);
+
+        globalVariable ++;
+        globalVariable --;
+//        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    }
+ }
+
+ void task2()
+ {
+    for(int i = 0; i < 100000; i++)
+    {
+        std::lock_guard<std::mutex> lock2(mtx2,std::adopt_lock);
+        std::lock_guard<std::mutex> lock1(mtx1,std::adopt_lock);
+        std::lock(mtx1,mtx2);
+        globalVariable ++;
+        globalVariable --;
+
+    }
+ }
 int main()
 {
-    std::cout <<"This is a CPP" << std::endl;
+    std::thread t1(task1);
+    std::thread t2(task2);
+    t1.join();
+    t2.join();
+    std::cout << globalVariable << std::endl;
     return 0;
 }
